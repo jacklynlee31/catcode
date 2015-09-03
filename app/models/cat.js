bd61f7cefd741a9ff8095a84312baecabbc2e13c
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-03 10:41:12 -0500

    Finished Styling the main page and the Level One page. Next I plan to work on the cat model.

--- app/models/cat.js
+++ app/models/cat.js
@@ -2,5 +2,10 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  hunger: DS.attr('number'),
+  x: 0,
+  y: 2,
+  // image: 'catwalk.png'
+  // url: "http"
 });
