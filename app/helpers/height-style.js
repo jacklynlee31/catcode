38a8a21b3c94f09569ca081aedfe9bcff9a971d5
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-09-14 14:08:17 -0500

    Working on levels, helpers

--- app/helpers/height-style.js
+++ app/helpers/height-style.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export function heightStyle(params/*, hash*/) {
-  return params;
+  var [maxX, maxY] = params;
+
+  return `width:${(1/(maxX + 1)) * 100}%; height:${(1/(maxY + 2)) * 100}%;`;
 }
 
 export default Ember.Helper.helper(heightStyle);
